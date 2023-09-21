6d92ef5e0b90cd024f055c84dfd053d3df3014e8
HTwoOhwater <710570212@qq.com>
2023-09-21 20:32:48 +0800

    正在重写

--- neuro_network.py
+++ neuro_network.py
@@ -1,12 +1,52 @@
 import torch
+import torch.nn as nn
 
 
-class Simple(torch.nn.Module):
+class Simple(nn.Module):
     def __init__(self):
         super(Simple, self).__init__()
-        self.fc1 = torch.nn.Linear(in_features=784, out_features=10)
+        self.fc1 = torch.nn.Linear(in_features=784, out_features=10, bias=True)
 
     def forward(self, x):
         return self.fc1(x)
 
 
+class AlexNet(nn.Module):
+    def __init__(self, config):
+        super(AlexNet, self).__init__()
+        self._config = config
+        # 定义卷积层和池化层
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        # 自适应层，将上一层的数据转换成6x6大小
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        # 全连接层
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, self._config['num_classes']),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
